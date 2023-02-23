eb5e8f824c65334fc9b71e06cc6feebe7c7a836c
2156153 <m.gagnon941@gmail.com>
2023-02-23 12:05:10 -0500

    feat: Create database, schemas, and tables

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,11 +1,13 @@
-
 -- █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
 -- █          Création de la BD          █
 -- █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
 
 
+CREATE DATABASE Consumerism
+GO
 
-
+USE Consumerism
+GO
 
 
 -- █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
@@ -13,11 +15,11 @@
 -- █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
 
 
+CREATE SCHEMA Offer
+GO
 
-
-
-
-
+CREATE SCHEMA Demand
+GO
 
 
 -- █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
@@ -25,10 +27,78 @@
 -- █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
 
 
+CREATE TABLE Offer.Enterprise
+(
+	EnterpriseID		INT			NOT NULL		IDENTITY(1,1),
+	Name				NVARCHAR(50)	NOT NULL		UNIQUE,
+	ProductionInterval	DATETIME		NOT NULL, 
 
+	CONSTRAINT 	PK_Enterprise_EnterpriseID 	PRIMARY KEY 	(EnterpriseID),
+)
 
+CREATE TABLE Offer.Contract
+(
+	ContractID		INT				NOT NULL		IDENTITY(1,1),
+	Name			NVARCHAR(50)		NOT NULL,
+	Importance		INT				NOT NULL,
+	EmployeeID		INT				NOT NULL,		-- FK
+	EnterpriseID	INT				NOT NULL,		-- FK
 
+	CONSTRAINT 	PK_Contract_ContractID 		PRIMARY KEY 	(ContractID),
+)
 
+CREATE TABLE Offer.Production
+(
+	ProductionID	INT				NOT NULL		IDENTITY(1,1),
+	Date			DATETIME			NOT NULL,		-- FK
+	Quantity		INT				NOT NULL,
+
+	CONSTRAINT 	PK_Production_ProductionID 	PRIMARY KEY 	(ProductionID),
+)
+
+CREATE TABLE Offer.Product
+(
+	ProductID		INT				NOT NULL		IDENTITY(1,1),
+	Name			NVARCHAR(50)		NOT NULL,
+	IsService		BIT				NOT NULL,
+	Value			MONEY				NOT NULL,
+	Complexity		INT				NOT NULL,
+
+	CONSTRAINT 	PK_Product_ProductID 		PRIMARY KEY 	(ProductID),
+)
+
+CREATE TABLE Offer.ProductInstance
+(
+	ProductInstanceID		INT					NOT NULL		IDENTITY(1,1),
+	ProductID			INT					NOT NULL,		-- FK
+	ProductionID		INT					NOT NULL,		-- FK
+
+	CONSTRAINT 	PK_ProductInstance_ProductInstanceID 	PRIMARY KEY 	(ProductInstanceID),
+)
+
+CREATE TABLE Demand.[Transaction]
+(
+	TransactionID		INT				NOT NULL		IDENTITY(1,1),
+	Date				DATETIME			NOT NULL,
+	Balance			MONEY				NOT NULL,
+	SenderID			INT				NOT NULL,		-- FK
+	ReceiverID			INT				NOT NULL,		-- FK
+	ProductInstanceID		INT				NOT NULL,		-- FK
+
+	CONSTRAINT 	PK_Transaction_TransactionID 		PRIMARY KEY 	(TransactionID),
+)
+
+CREATE TABLE Demand.Bozo
+(
+	BozoID	INT			NOT NULL		IDENTITY(1,1),
+	FirstName	NVARCHAR(50)	NOT NULL,
+	LastName	NVARCHAR(50)	NOT NULL,
+	Nickname	NVARCHAR(50)	NOT NULL,
+	Honesty	INT			NOT NULL,
+	
+	CONSTRAINT 	PK_Bozo_BozoID 	PRIMARY KEY 	(BozoID)
+)
+GO
 
 
 -- █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
@@ -42,3 +112,4 @@
 -- █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
 -- █      Création des contraintes UC,DF,CK     █
 -- █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+
---
+++ README.md
@@ -0,0 +1,4 @@
+## Tip
+It's much easier to use VS code.  
+Simply install the official "SQL Server" extensions bundle from Microsoft.  
+You can create a connection and execute queries right from the editor.
