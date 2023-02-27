064a5e74ca4f68ca4c82fc0d058cbb88948fae67
2156153 <m.gagnon941@gmail.com>
2023-02-27 11:14:59 -0500

    feat: Add foreign key constraints

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -2,6 +2,9 @@
 -- █          Création de la BD          █
 -- █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
 
+USE master
+DROP DATABASE Consumerism
+GO
 
 CREATE DATABASE Consumerism
 GO
@@ -26,45 +29,45 @@ GO
 -- █ Création des tables + contraintes (id, null?, pk) █
 -- █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
 
-
-CREATE TABLE Offer.Enterprise
+CREATE TABLE Demand.Bozo
 (
-	EnterpriseID		INT			NOT NULL		IDENTITY(1,1),
-	Name				NVARCHAR(50)	NOT NULL		UNIQUE,
-	ProductionInterval	DATETIME		NOT NULL, 
-
-	CONSTRAINT 	PK_Enterprise_EnterpriseID 	PRIMARY KEY 	(EnterpriseID),
+	BozoID	INT			NOT NULL		IDENTITY(1,1),
+	FirstName	NVARCHAR(50)	NOT NULL,
+	LastName	NVARCHAR(50)	NOT NULL,
+	Nickname	VARCHAR(50)		NOT NULL,		-- no weird characters in nickname
+	Honesty	INT			NOT NULL,		-- CK
+	
+	CONSTRAINT 	PK_Bozo_BozoID 	PRIMARY KEY 	(BozoID)
 )
 
 CREATE TABLE Offer.Contract
 (
 	ContractID		INT				NOT NULL		IDENTITY(1,1),
-	Name			NVARCHAR(50)		NOT NULL,
-	Importance		INT				NOT NULL,
+	Description		NVARCHAR(75)		NOT NULL,
+	Importance		INT				NOT NULL,		-- CK (>= 0, <= 100)
 	EmployeeID		INT				NOT NULL,		-- FK
 	EnterpriseID	INT				NOT NULL,		-- FK
 
 	CONSTRAINT 	PK_Contract_ContractID 		PRIMARY KEY 	(ContractID),
 )
 
+CREATE TABLE Offer.Enterprise
+(
+	EnterpriseID		INT			NOT NULL		IDENTITY(1,1),
+	Name				NVARCHAR(50)	NOT NULL,		-- UC
+	ProductionInterval	DATETIME		NOT NULL, 
+
+	CONSTRAINT 	PK_Enterprise_EnterpriseID 	PRIMARY KEY 	(EnterpriseID),
+)
+
 CREATE TABLE Offer.Production
 (
 	ProductionID	INT				NOT NULL		IDENTITY(1,1),
-	Date			DATETIME			NOT NULL,		-- FK
-	Quantity		INT				NOT NULL,
+	Date			DATETIME			NOT NULL,
+	Quantity		INT				NOT NULL,		-- CK (>= 0)
+	ContractID		INT				NOT NULL,		-- FK
 
 	CONSTRAINT 	PK_Production_ProductionID 	PRIMARY KEY 	(ProductionID),
-)
-
-CREATE TABLE Offer.Product
-(
-	ProductID		INT				NOT NULL		IDENTITY(1,1),
-	Name			NVARCHAR(50)		NOT NULL,
-	IsService		BIT				NOT NULL,
-	Value			MONEY				NOT NULL,
-	Complexity		INT				NOT NULL,
-
-	CONSTRAINT 	PK_Product_ProductID 		PRIMARY KEY 	(ProductID),
 )
 
 CREATE TABLE Offer.ProductInstance
@@ -80,7 +83,7 @@ CREATE TABLE Demand.[Transaction]
 (
 	TransactionID		INT				NOT NULL		IDENTITY(1,1),
 	Date				DATETIME			NOT NULL,
-	Balance			MONEY				NOT NULL,
+	Amount			MONEY				NOT NULL,		-- CK (>= 0)
 	SenderID			INT				NOT NULL,		-- FK
 	ReceiverID			INT				NOT NULL,		-- FK
 	ProductInstanceID		INT				NOT NULL,		-- FK
@@ -88,24 +91,63 @@ CREATE TABLE Demand.[Transaction]
 	CONSTRAINT 	PK_Transaction_TransactionID 		PRIMARY KEY 	(TransactionID),
 )
 
-CREATE TABLE Demand.Bozo
+CREATE TABLE Offer.Product
 (
-	BozoID	INT			NOT NULL		IDENTITY(1,1),
-	FirstName	NVARCHAR(50)	NOT NULL,
-	LastName	NVARCHAR(50)	NOT NULL,
-	Nickname	NVARCHAR(50)	NOT NULL,
-	Honesty	INT			NOT NULL,
-	
-	CONSTRAINT 	PK_Bozo_BozoID 	PRIMARY KEY 	(BozoID)
+	ProductID		INT				NOT NULL		IDENTITY(1,1),
+	Name			NVARCHAR(50)		NOT NULL,
+	IsService		BIT				NOT NULL,
+	BaseValue		MONEY				NOT NULL,	
+	Complexity		INT				NOT NULL,		-- CK (>= 0, <= 100)
+
+	CONSTRAINT 	PK_Product_ProductID 		PRIMARY KEY 	(ProductID),
 )
+
 GO
+
 
 
 -- █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
 -- █ Création des contraintes de clé étrangère █
 -- █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
 
+ALTER TABLE Offer.Contract
+ADD CONSTRAINT FK_Contract_BozoID FOREIGN KEY (EmployeeID) REFERENCES Demand.Bozo(BozoID) 
+ON UPDATE CASCADE
+ON DELETE CASCADE -- contract binds an employee to an enterprise
 
+ALTER TABLE Offer.Contract
+ADD CONSTRAINT FK_Contract_EnterpriseID FOREIGN KEY (EnterpriseID) REFERENCES Offer.Enterprise(EnterpriseID)
+ON UPDATE CASCADE
+ON DELETE CASCADE -- contract binds an employee to an enterprise
+
+ALTER TABLE Offer.Production
+ADD CONSTRAINT FK_Production_ContractID FOREIGN KEY (ContractID) REFERENCES Offer.Contract(ContractID)
+ON UPDATE CASCADE
+ON DELETE CASCADE -- production must be bound to a contract
+
+ALTER TABLE Offer.ProductInstance
+ADD CONSTRAINT FK_ProductInstance_ProductID FOREIGN KEY (ProductID) REFERENCES Offer.Product(ProductID)
+ON UPDATE CASCADE
+ON DELETE CASCADE -- product instance cannot exist without a product
+
+ALTER TABLE Offer.ProductInstance
+ADD CONSTRAINT FK_ProductInstance_ProductionID FOREIGN KEY (ProductionID) REFERENCES Offer.Production(ProductionID)
+ON UPDATE CASCADE
+ON DELETE CASCADE -- product instance must have been produced
+
+ALTER TABLE Demand.[Transaction]
+ADD CONSTRAINT FK_Transaction_SenderID FOREIGN KEY (SenderID) REFERENCES Demand.Bozo(BozoID)
+ON UPDATE CASCADE
+ON DELETE CASCADE -- transaction must have a sender
+
+-- handling cascades is too complicated for the two following since SQL Server only supports 1 cascade for the same referenced key
+ALTER TABLE Demand.[Transaction]
+ADD CONSTRAINT FK_Transaction_ReceiverID FOREIGN KEY (ReceiverID) REFERENCES Demand.Bozo(BozoID)
+
+ALTER TABLE Demand.[Transaction]
+ADD CONSTRAINT FK_Transaction_ProductInstanceID FOREIGN KEY (ProductInstanceID) REFERENCES Offer.ProductInstance(ProductInstanceID)
+
+GO
 
 
 
