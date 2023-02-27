796ad4df94399dda18e0b8526a35ff53d79f2d83
2156153 <m.gagnon941@gmail.com>
2023-02-27 14:54:11 -0500

    feat: Add something

--- InsertSeedData.sql
+++ InsertSeedData.sql
@@ -12,10 +12,15 @@ GO
 -- █     Insertion de données dans les tables     █
 -- █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
 
+-- global variable for max number of generated data
 EXEC sp_set_session_context 'max', 100
 GO
 
 -- obtient le nombre de rangees d'une table
+IF OBJECT_ID('dbo.TABLESIZE') IS NOT NULL
+  DROP FUNCTION dbo.TABLESIZE
+GO
+
 CREATE FUNCTION dbo.TABLESIZE(@tableName NVARCHAR(128))
 RETURNS INT
 AS
@@ -30,6 +35,10 @@ END
 GO
 
 -- Math.max()
+IF OBJECT_ID('dbo.MAX') IS NOT NULL
+  DROP FUNCTION dbo.MAX
+GO
+
 CREATE FUNCTION dbo.MAX(@a INT, @b INT)
 RETURNS INT
 AS
@@ -84,3 +93,64 @@ BEGIN
 END
 GO
 
+-- offer.production
+DECLARE @i INT = dbo.TABLESIZE(N'Offer.Production')
+DECLARE @max INT = @i + CONVERT(INT, SESSION_CONTEXT(N'max'))
+WHILE @i < @max
+BEGIN
+      INSERT INTO Offer.Production
+      VALUES (
+            DATEADD(DAY, RAND() * 1000, '2000-01-01T00:00:00'),
+            RAND() * 100000, 
+            dbo.MAX(1, RAND() * @max)
+      )      
+      SET @i = @i + 1
+END
+GO
+
+-- offer.product
+DECLARE @i INT = dbo.TABLESIZE(N'Offer.Product')
+DECLARE @max INT = @i + CONVERT(INT, SESSION_CONTEXT(N'max'))
+WHILE @i < @max
+BEGIN
+      INSERT INTO Offer.Product
+      VALUES (
+            'Name ' + LTRIM(@i),
+            (CASE WHEN RAND() < 0.5 THEN 1 ELSE 0 END),
+            RAND() * 1000000, 
+            RAND() * 100
+      )      
+      SET @i = @i + 1
+END
+GO
+
+-- offer.productinstance
+DECLARE @i INT = dbo.TABLESIZE(N'Offer.ProductInstance')
+DECLARE @max INT = @i + CONVERT(INT, SESSION_CONTEXT(N'max'))
+WHILE @i < @max
+BEGIN
+      INSERT INTO Offer.ProductInstance
+      VALUES (
+            dbo.MAX(1, RAND() * @max),
+            dbo.MAX(1, RAND() * @max)
+      )      
+      SET @i = @i + 1
+END
+GO
+
+-- demand.transaction
+DECLARE @i INT = dbo.TABLESIZE(N'Demand.Transaction')
+DECLARE @max INT = @i + CONVERT(INT, SESSION_CONTEXT(N'max'))
+WHILE @i < @max
+BEGIN
+      INSERT INTO Demand.[Transaction]
+      VALUES (
+            DATEADD(DAY, RAND() * 1000, '2000-01-01'),
+            RAND() * 1000000, 
+            dbo.MAX(1, RAND() * @max),
+            dbo.MAX(1, RAND() * @max),
+            dbo.MAX(1, RAND() * @max)
+      )      
+      SET @i = @i + 1
+END
+GO
