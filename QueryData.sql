749e01d7af8506c05fcd826153aa2b10de1cfbff
2156153 <m.gagnon941@gmail.com>
2023-02-27 17:51:27 -0500

    feat: First query

--- QueryData.sql
+++ QueryData.sql
@@ -11,6 +11,41 @@ GO
 -- █     5 requêtes dont une avec une sous-requête     █
 -- █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
 
-SELECT EnterpriseID, EmployeeID
-from Offer.Contract
-group by EnterpriseID, EmployeeID
+-- Obtient la liste des entreprises 
+-- dont les employes ont une honnetete moyenne superieure a 60
+-- ============================================================
+-- Name           AverageEmployeeHonesty
+-- ------------------------------------------------------------
+-- Dynabox	      70
+-- Yoveo	      67
+-- Skiba	      67
+-- Lazz	      66
+-- Dabshots	      65
+-- Meevee	      63
+-- Realpoint      62
+-- Skinte	      62
+-- Aibox	      62
+-- Thoughtstorm   61
+-- Roomm	      61
+-- Zava	      61
+-- Geba	      61
+
+SELECT E.Name, AVG(B.Honesty) AS AverageEmployeeHonesty
+FROM Offer.Contract as C
+INNER JOIN Demand.Bozo as B
+ON C.EmployeeID = B.BozoID
+INNER JOIN Offer.Enterprise as E
+ON C.EnterpriseID = E.EnterpriseID
+GROUP BY C.EnterpriseID, E.Name
+HAVING AVG(B.Honesty) > 60
+ORDER BY AVG(B.Honesty) DESC
+GO
+
+SELECT Amount, Date
+FROM Demand.[Transaction] AS T
+INNER JOIN Offer.ProductInstance AS PI
+ON T.ProductInstanceID = PI.ProductInstanceID
+WHERE PI.ProductID IN (SELECT ProductID
+           FROM Offer.Product as P
+           WHERE P.IsService = 1
+           AND P.BaseValue > 500000)
