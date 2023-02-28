5b9730c95990a9e7d4d80b16a9d0e765923b9130
2156153 <m.gagnon941@gmail.com>
2023-02-28 13:08:47 -0500

    feat: Add two first requests

--- QueryData.sql
+++ QueryData.sql
@@ -11,28 +11,48 @@ GO
 -- █     5 requêtes dont une avec une sous-requête     █
 -- █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
 
-SELECT E.Name, AVG(B.Honesty) AS AverageEmployeeHonesty
+
+
+-- LE GRAIN DE L'IVRAIE
+-- ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+-- Obtient le nom des entreprises dont l'honnêteté moyenne des employés est supérieure à 50,
+-- triées par ordre décroissant de cette honnêteté moyenne.
+
+-- NOTE: Il est possible que cette requête ne retourne aucun résultat si les données de test
+--       ne contiennent pas d'employés honnêtes. Dans ce cas désolant, il suffit de changer
+--       la valeur 50 à une valeur plus basse.
+SELECT 
+      E.Name as 'Nom de l''entreprise', 
+      AVG(B.Honesty) AS 'Honnêteté moyenne des employés'
 FROM Offer.Contract as C
 INNER JOIN Demand.Bozo as B
-ON C.EmployeeID = B.BozoID
+      ON C.EmployeeID = B.BozoID
 INNER JOIN Offer.Enterprise as E
-ON C.EnterpriseID = E.EnterpriseID
+      ON C.EnterpriseID = E.EnterpriseID
 GROUP BY C.EnterpriseID, E.Name
 HAVING AVG(B.Honesty) > 50
 ORDER BY AVG(B.Honesty) DESC
 GO
 
--- Obtient le nom, le prix, et la date de transaction des 10 premiers produits (s'il y en a) 
--- achetés de type service dont la valeur de base (sans taxes et frais de livraison)
+-- MARKET LAWS
+-- ▀▀▀▀▀▀▀▀▀▀▀
+-- Obtient le nom, le prix, et la date de transaction des 10 premiers produits 
+-- achetés n'étant pas un service dont la valeur de base (sans taxes et frais de livraison)
 -- est supérieure à 500 000$, triés par ordre décroissant de prix.
-SELECT TOP 10 P.Name as 'Nom du produit', LTRIM(ROUND(Amount, 2)) + ' $' as 'Montant avec taxes', FORMAT(Date, 'yyyy-MM-dd à HH:mm:ss') as 'Date-heure transaction'
+
+-- NOTE: Il est possible qu'il y ait moins de 10 produits qui correspondent à ces critères.
+SELECT TOP 10 
+      P.Name as 'Nom du produit', 
+      LTRIM(ROUND(Amount, 2)) + ' $' as 'Montant avec taxes', 
+      FORMAT(Date, 'yyyy-MM-dd à HH:mm:ss') as 'Date-heure transaction'
 FROM Demand.[Transaction] AS T
 INNER JOIN Offer.ProductInstance AS PI
-ON T.ProductInstanceID = PI.ProductInstanceID
+      ON T.ProductInstanceID = PI.ProductInstanceID
 INNER JOIN Offer.Product AS P
-ON PI.ProductID = P.ProductID
+      ON PI.ProductID = P.ProductID
 WHERE PI.ProductID IN (SELECT ProductID
            FROM Offer.Product as P
-           WHERE P.IsService = 1
+           WHERE P.IsService = 0
            AND P.BaseValue > 500000)
 ORDER BY Amount DESC
+GO
